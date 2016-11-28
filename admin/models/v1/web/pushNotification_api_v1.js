b4c5af32c5eaf3096549ce2f2d314c2299fd62ee
Daniel Park <dpark1985@gmail.com>
2016-11-28 15:10:04 +0900

    [push]

--- admin/models/v1/web/pushNotification_api_v1.js
+++ admin/models/v1/web/pushNotification_api_v1.js
@@ -7,7 +7,7 @@ const express           = require('express');
 const router            = express.Router();
 const https             = require('https');
 
-const apiUrl            = "http://175.206.34.229:2016/";
+const serverIp          = require('../../../config/serverConfig');
 const FCM_TOCKEN        = 'AAAAmok3Qu0:APA91bEzrwaa-T0pXm8kIP86RjirdQ_Sd5bW8tjHZSoXNi12OK96vkg0NAMhRyqrOJez-o0uG5hyb4UQlW3dJH_saehpANEn6ZQpzxel9ZdWne_xl2NO3GpmgBm_oFdVGiN71BJQGZiZKWkDUJX18M5oeyJzA0YjDw';
 const SERVER_KEY        = 'AIzaSyAD0yNvn1TVUP9oxkCqS73JI6K_wdzm3fQ';
 
@@ -28,10 +28,10 @@ router.post('/doPush', function (req, res, next) {
   req.db.devices.find({}, function (err, data) {
     if(err) res.json({pushNotification: false});
 
-    req.body.imgThumbSrc[0].src = apiUrl + req.body.imgThumbSrc[0].src;
+    req.body.imgThumbSrc[0].src = serverIp.getServerIp() + req.body.imgThumbSrc[0].src;
 
     for(var k=0; k<req.body.imgSeriesSrc.length; k++){
-      req.body.imgSeriesSrc[k].src = apiUrl + req.body.imgSeriesSrc[k].src;
+      req.body.imgSeriesSrc[k].src = serverIp.getServerIp() + req.body.imgSeriesSrc[k].src;
     }
 
     for(var i=0; i<data.length; i++){
